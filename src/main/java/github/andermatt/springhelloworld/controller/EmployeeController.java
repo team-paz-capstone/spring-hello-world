32fe59c26212d3d8064244889e974a8c51ff71e0
andermatt <matt10.anderson@gmail.com>
2019-04-14 12:41:55 -0400

    Added endpoint to create new employees.

--- src/main/java/github/andermatt/springhelloworld/controller/EmployeeController.java
+++ src/main/java/github/andermatt/springhelloworld/controller/EmployeeController.java
@@ -6,7 +6,9 @@ import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController()
@@ -58,5 +60,35 @@ public class EmployeeController {
         return employeeRepository.findByEmail(email)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+
+/*
+Create a new employee. POST JSON to this endpoint to create a new employee:
+    {
+        "firstName": "my name",
+        "lastName": "my last name",
+        "email": "my email"
+    }
+
+    The response body contains the newly created employee, and the response header contains the URI
+    location of the newly created employee resource.
+ */
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    @ApiOperation(value = "Create a new Employee")
+    @ApiResponse(code = 201, message = "Successfully created new employee.")
+    public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee) {
+
+        // In our real implementation, we'll need to do some validation. Also, an employee would have a password -
+        // the password needs to be hashed before saving to the database.
+
+        Employee savedEmployee = employeeRepository.save(employee);
+        Long idOfNewResource = savedEmployee.getId();
+
+        // Include the location of the new resource in the CREATED response ("Location" response header.)
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{idOfNewResource}").buildAndExpand(idOfNewResource).toUri();
+
+        return ResponseEntity.created(uriOfNewResource).body(savedEmployee);
     }
 }
